bea0b37c6cfc134ac8a5b05182b602291d480e98
GeneGulanesJr <ggulanes_180000000405@uic.edu.ph>
2022-03-09 18:04:49 +0800

    Boiler Plate Done

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import {
     ChakraProvider,
     theme,
 } from '@chakra-ui/react';
-import {BrowserRouter  as Router,Route,Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import RegisterPage from './Pages/RegisterPage';
 import HomePage from './Pages/Content/Homepage'
 
 import Navbar from "./Component/Navbar";
+import Footer from "./Component/Footer";
 import LoginPage from "./Pages/LoginPage";
+import Body from "./Pages/Content/Body";
+import Review from "./Pages/Content/Review";
+import Pricing from "./Pages/Content/Pricing";
 
 function App() {
     return (
 
-            <Router>
-                <ChakraProvider theme={theme}>
+        <Router>
+            <ChakraProvider theme={theme}>
                 <Navbar/>
+
+
                 <Routes>
                     <Route path="/" element={<HomePage/>}/>
                     <Route path="/register" element={<RegisterPage/>}/>
                     <Route path="/login" element={<LoginPage/>}/>
                 </Routes>
-                </ChakraProvider>
-            </Router>
-
+                <Body/>
+                <Review/>
+                <Pricing/>
+                <Footer/>
+            </ChakraProvider>
+        </Router>
 
 
     );
